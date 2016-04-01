08aed516faaf8ea89467a5df0879ddaf9417def3
Pareek Ravi <ravip2@mcmaster.ca>
2016-04-01 00:19:40 -0400

    data 86-87

--- sql_test.rb
+++ sql_test.rb
@@ -5,31 +5,43 @@ require 'csv'
 
 begin
 
-  puts "Started"
   db = SQLite3::Database.open "California.db"
   # puts db.get_first_value 'SELECT SQLITE_VERSION()'
 
   db.execute "CREATE TABLE IF NOT EXISTS Station (Id String PRIMARY KEY,
          Name TEXT, Latitude DOUBLE, Longitude DOUBLE, Elevation DOUBLE)"
-  CSV.foreach('data/california.csv', headers:true) do |row|
-    id   = row[0].to_s
-    name = row[1].to_s
-    elev = row[2].to_f
-    lat  = row[3].to_f
-    long = row[4].to_f
 
-    params = id, name, lat, long, elev
+  dirArray = Dir.entries("ORIGNAL DATA")
+  dirArray = dirArray[2..dirArray.length]
 
-    stm = db.execute "SELECT COUNT(Id) AS STATIONEXISTS FROM Station WHERE Id=?",id
-    # puts stm[0][0]
+  readCSVs =
+  # puts dirArray
 
-    if stm[0][0] == 0
-      db.execute "INSERT INTO Station VALUES(?,?,?,?,?)", params
-      puts id.to_s + " " + name.to_s + " " + long.to_s + " " + lat.to_s + " " + elev.to_s
+  puts "Started"
+  for i in dirArray
+    puts i
+    CSV.foreach('ORIGNAL DATA/' + i, headers: true) do |row|
+      id = row[0].to_s
+      name = row[1].to_s
+      elev = row[2].to_f
+      lat = row[3].to_f
+      long = row[4].to_f
+
+      params = id, name, lat, long, elev
+
+      stm = db.execute "SELECT COUNT(Id) AS STATIONEXISTS FROM Station WHERE Id=?", id
+      # puts stm[0][0]
+
+      if stm[0][0] == 0
+        db.execute "INSERT INTO Station VALUES(?,?,?,?,?)", params
+        # puts id.to_s + " " + name.to_s + " " + long.to_s + " " + lat.to_s + " " + elev.to_s
+      end
     end
   end
 
-
+  open('Used Data.txt', 'a') do
+      |f| f.puts dirArray
+  end
   puts "Done"
 
 rescue SQLite3::Exception => e
