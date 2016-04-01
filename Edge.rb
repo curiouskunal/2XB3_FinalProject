6e6c42fd5a68f3e2b707eda7ae9858231cdb995f
Pavithran Pathmarajah <Pavithranpath@gmail.com>
2016-04-01 11:06:08 -0400

    Edge Cross Check

--- Edge.rb
+++ Edge.rb
@@ -8,9 +8,9 @@ class Edge
     @s1=node1
     @s2=node2
     #pre-calculate all values
-    @tempDays = PercentTempDays(@s1.weather,@s2.weather,tolerance)
-    @rainDays = PercentRainDays(@s1.weather,@s2.weather,tolerance)
-    @length = distanceCalc(@s1,@s2)
+    #@tempDays = PercentTempDays(@s1.weather,@s2.weather,tolerance)
+    #@rainDays = PercentRainDays(@s1.weather,@s2.weather,tolerance)
+    #@length = distanceCalc(@s1,@s2)
   end
 
   def PercenTemptDays(val1,val2,tolerance)
@@ -44,13 +44,22 @@ class Edge
     return @s1, @s2
   end
 
-  def cross()
-    
-    return true;
-  end
-
   def getWeather()
     return @tempDays, @rainDays
+  end
+
+  def distanceCalc(s1, s2)
+    radiusEarth=6371000;
+    x1 = s1.location.longitude*(Math::PI/180.0)
+    x2 = s2.location.longitude*(Math::PI/180.0)
+    y1 = s1.location.latitude*(Math::PI/180.0)
+    y2 = s2.location.latitude*(Math::PI/180.0)
+    deltaX = x1-x2
+    deltaY=y1-y2
+    a = Math.sin(deltaX/2.0) * Math.sin(deltaX/2.0) + Math.cos(x1) * Math.cos(x2) * Math.sin(deltaY/2.0)* Math.sin(deltaY/2.0)
+    circumfrence = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1.0-a))
+
+    return radiusEarth * circumfrence
   end
 
   def self.distanceCalc(s1, s2)
@@ -73,5 +82,55 @@ class Edge
 
   def >= (other)
     return @length>=other.distance()
+  end
+
+
+  def self.cross(vals1, vals2)
+    if (vals2==nil)
+      return checkPoints(@s1,@s2,vals1.s1,vals1.s2)
+    end
+    return true;
+  end
+
+  def cross(vals1)
+    if vals1.is_a?(Edge)
+      tmp1,tmp2=vals1.getNodes()
+      return Edge.checkPoints(@s1,@s2,tmp1,tmp2)
+    else
+      doesCross=false
+      vals1.each do |tmp|
+        tmp1,tmp2=tmp.getNodes()
+        if (!doesCross)
+         doesCross = Edge.checkPoints(@s1,@s2,tmp1,tmp2)
+        end
+      end
+      return doesCross
+    end
+  end
+
+  def self.checkPoints(s1,s2,s3,s4)
+    x1=s1.location.latitude*(Math::PI/180.0)
+    x2=s2.location.latitude*(Math::PI/180.0)
+    x3=s3.location.latitude*(Math::PI/180.0)
+    x4=s4.location.latitude*(Math::PI/180.0)
+    y1=s1.location.longitude*(Math::PI/180.0)
+    y2=s2.location.longitude*(Math::PI/180.0)
+    y3=s3.location.longitude*(Math::PI/180.0)
+    y4=s4.location.longitude*(Math::PI/180.0)
+
+    px=(((x1*y1-y1*x2)*(x3-x4))-((x1-x2)*(x3*y4-y3*x4)))/(((x1-x2)*(y3-y4))-(y1-y2)*(x3-x4))
+    py=(((x1*y2-y1*x2)*(y3-y4))-((y1-y2)*(x3*y4-y3*x4)))/(((x1-x2)*(y3-y4))-((y1-y2)*(x3-x4)))
+
+    det = ((x1-x2)*(y3-y4))-((y1-y2)*(x3-x4))
+    if (det ==0 )
+      return false;
+    else
+     if (((x1<=px&&px<=x2)||(x2<=px&&px<=x1))&&((x3<=px&&px<=x4)||(x4<=px&&px<=x3)))
+       if (((y1<=py&&py<=y2)||(y2<=py&&py<=y1))&&((y3<=py&&py<=y4)||(y4<=py&&py<=y3)))
+       return true
+       end
+     end
+      return false
+    end
   end
 end
